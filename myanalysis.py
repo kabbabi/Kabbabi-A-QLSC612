905f15887e2de6f332e6e23b1acee683cacfe478
Achraf <achraf.kabbabi@gmail.com>
2020-05-20 22:13:52 -0400

    Update myanalysis.py

--- myanalysis.py
+++ myanalysis.py
@@ -1,2 +1,10 @@
 # myanalysis.py
 import numpy as np
+
+# Read "brainsize.csv" from "../practical/brainsize.csv"
+
+# Add a first random variable partY
+
+# Add a second random variable partY2
+
+# Visualizations
